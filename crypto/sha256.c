50603545dd616da4aaf2a606f25d6fc3f4fdf098
Arthur Damm <indigoarthur@gmail.com>
2020-09-04 03:49:14 +0000

    Add documentation to function

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,5 +1,12 @@
 #include "hblk_crypto.h"
 
+/**
+ * sha256 - gets SHA256 hash for given string
+ * @s: the string to encode
+ * @len: the length of given string
+ * @digest: buffer for hash
+ * Return: pointer to digest else NULL
+ */
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
