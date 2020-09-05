0f33bc9aca3f03cb2d3e2524d8c5fc8806ace121
Arthur Damm <indigoarthur@gmail.com>
2020-09-05 05:20:53 +0000

    Add task 4. EC_KEY - Save to file

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -7,11 +7,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/stat.h>
+#include <sys/types.h>
+
 #include <openssl/sha.h>
 #include <openssl/ec.h>
 /* #include <openssl/bn.h> */
 #include <openssl/obj_mac.h>
-
 #include <openssl/bio.h>
 #include <openssl/err.h>
 #include <openssl/pem.h>
@@ -50,5 +52,7 @@ EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+
+int ec_save(EC_KEY *key, char const *folder);
 
 #endif
