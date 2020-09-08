9eba6dffd10906194bc704ad8106a58adbeae882
Arthur Damm <indigoarthur@gmail.com>
2020-09-08 20:26:14 +0000

    Ingore len 0

--- crypto/sha256.c
+++ crypto/sha256.c
@@ -10,7 +10,7 @@
 uint8_t *sha256(int8_t const *s, size_t len,
 	uint8_t digest[SHA256_DIGEST_LENGTH])
 {
-	if (!s || !len || !digest)
+	if (!s || !digest)
 		return (NULL);
 	return (SHA256((const unsigned char *)s, len, digest));
 }
