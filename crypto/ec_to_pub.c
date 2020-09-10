b84abc9df1d7f213dd464b52ea2dbb450a045899
Arthur Damm <indigoarthur@gmail.com>
2020-09-10 02:17:07 +0000

    Check not len only

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -23,7 +23,7 @@ uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 		point,
 		POINT_CONVERSION_UNCOMPRESSED,
 		pub, EC_PUB_LEN, NULL);
-	if (len != EC_PUB_LEN)
+	if (!len)
 		return (NULL);
 	return (pub);
 }
