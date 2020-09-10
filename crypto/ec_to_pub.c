1dd31230fac22d989702362892ed5566e5b22c6c
Arthur Damm <indigoarthur@gmail.com>
2020-09-10 02:09:13 +0000

    Set vars to null

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -8,8 +8,8 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	EC_GROUP const *group;
-	EC_POINT const *point;
+	EC_GROUP const *group = NULL;
+	EC_POINT const *point = NULL;
 	size_t len;
 
 	if (!key)
