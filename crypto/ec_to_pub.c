3ddf8fffcb16c60c2731482f66e3fcd6c68f6b86
Arthur Damm <indigoarthur@gmail.com>
2020-09-10 02:07:21 +0000

    Check returns of functions

--- crypto/ec_to_pub.c
+++ crypto/ec_to_pub.c
@@ -8,9 +8,19 @@
  */
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN])
 {
-	size_t len = EC_POINT_point2oct(
-		EC_KEY_get0_group(key),
-		EC_KEY_get0_public_key(key),
+	EC_GROUP const *group;
+	EC_POINT const *point;
+	size_t len;
+
+	if (!key)
+		return (NULL);
+	group = EC_KEY_get0_group(key);
+	point = EC_KEY_get0_public_key(key);
+	if (!group || !point)
+		return (NULL);
+	len = EC_POINT_point2oct(
+		group,
+		point,
 		POINT_CONVERSION_UNCOMPRESSED,
 		pub, EC_PUB_LEN, NULL);
 	if (len != EC_PUB_LEN)
