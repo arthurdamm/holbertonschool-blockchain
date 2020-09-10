2c6dc2372a40bdce5fb72e7ffbfcc607ad178183
Arthur Damm <indigoarthur@gmail.com>
2020-09-10 19:14:23 +0000

    Restore previous ec_save

--- crypto/ec_save.c
+++ crypto/ec_save.c
@@ -8,42 +8,34 @@
  */
 int ec_save(EC_KEY *key, char const *folder)
 {
+	FILE *fp;
 	char path[128] = {0};
-	BIO *out;
 
 	if (!key || !folder)
 		return (0);
 	mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
 
 	sprintf(path, "%s/key.pem", folder);
-	out = BIO_new(BIO_s_file());
-	if (!out)
+	fp = fopen(path, "w");
+	if (!fp)
 		return (0);
-	BIO_set_close(out, BIO_CLOSE);
-	if (!BIO_write_filename(out, path))
-		return (BIO_free_all(out), 0);
-	if (!PEM_write_bio_ECPrivateKey(out, key, NULL, NULL, 0, NULL, NULL))
+	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
 	{
-		BIO_free_all(out);
+		fclose(fp);
 		return (0);
 	}
-	BIO_free_all(out);
+	fclose(fp);
 
 	sprintf(path, "%s/key_pub.pem", folder);
-	out = BIO_new(BIO_s_file());
-
-	if (!out)
+	fp = fopen(path, "w");
+	if (!fp)
 		return (0);
-	BIO_set_close(out, BIO_CLOSE);
-	if (!BIO_write_filename(out, path))
-		return (BIO_free_all(out), 0);
-	
-	if (!PEM_write_bio_EC_PUBKEY(out, key))
+	if (!PEM_write_EC_PUBKEY(fp, key))
 	{
-		BIO_free_all(out);
+		fclose(fp);
 		return (0);
 	}
-	BIO_free_all(out);
+	fclose(fp);
 
 	return (1);
 }
