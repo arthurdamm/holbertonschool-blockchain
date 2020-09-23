5bca76cb342b77a5731579a96d74df6de72cf42c
Arthur Damm <indigoarthur@gmail.com>
2020-09-23 23:46:37 +0000

    Add task 7

--- blockchain/v0.1/blockchain.h
+++ blockchain/v0.1/blockchain.h
@@ -91,6 +91,23 @@ typedef struct block_s
 	uint8_t	 hash[SHA256_DIGEST_LENGTH];
 } block_t;
 
+/* GENESIS BLOCK - first block in the chain */
+#define GENESIS_BLOCK { \
+	{ /* info */ \
+		0 /* index */, \
+		0, /* difficulty */ \
+		1537578000, /* timestamp */ \
+		0, /* nonce */ \
+		{0} /* prev_hash */ \
+	}, \
+	{ /* data */ \
+		"Holberton School", /* buffer */ \
+		16 /* len */ \
+	}, \
+	"\xc5\x2c\x26\xc8\xb5\x46\x16\x39\x63\x5d\x8e\xdf\x2a\x97\xd4\x8d" \
+	"\x0c\x8e\x00\x09\xc8\x17\xf2\xb1\xd3\xd7\xff\x2f\x04\x51\x58\x03" \
+}
+
 blockchain_t *blockchain_create(void);
 block_t *block_create(block_t const *prev, int8_t const *data,
 	uint32_t data_len);
@@ -101,5 +118,6 @@ uint8_t *block_hash(block_t const *block,
 int blockchain_serialize(blockchain_t const *blockchain, char const *path);
 blockchain_t *blockchain_deserialize(char const *path);
 llist_t *deserialize_blocks(int fd, uint32_t size, uint8_t endianness);
+int block_is_valid(block_t const *block, block_t const *prev_block);
 
 #endif
