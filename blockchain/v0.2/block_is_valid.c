f8bacc94aadec6b8a91efeba797f119c57f61bc7
Arthur Damm <indigoarthur@gmail.com>
2020-10-03 19:44:42 +0000

    Add task 1

--- blockchain/v0.2/block_is_valid.c
+++ blockchain/v0.2/block_is_valid.c
@@ -27,5 +27,7 @@ int block_is_valid(block_t const *block, block_t const *prev_block)
 		return (1);
 	if (block->data.len > BLOCKCHAIN_DATA_MAX)
 		return (1);
+	if (!hash_matches_difficulty(block->hash, block->info.difficulty))
+		return (1);
 	return (0);
 }
