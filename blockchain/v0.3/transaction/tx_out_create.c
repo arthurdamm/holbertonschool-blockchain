381d7f9f93972864d8530ca38d999ed1fc4aa720
Arthur Damm <indigoarthur@gmail.com>
2020-10-24 17:17:02 +0000

    Use buffer for hash

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -9,13 +9,16 @@
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
 	tx_out_t *t = calloc(1, sizeof(*t));
-	size_t len = sizeof(t->amount) + sizeof(t->pub);
+	unsigned char buf[sizeof(t->amount) + sizeof(t->pub)];
 
 	if (!t)
 		return (NULL);
 	t->amount = amount;
 	memcpy(t->pub, pub, sizeof(t->pub));
-	if (!sha256((int8_t const *)&t, len, t->hash))
+	memcpy(buf, &amount, sizeof(amount));
+	memcpy(buf + sizeof(amount), pub, EC_PUB_LEN);
+	if (!sha256((int8_t const *)buf, sizeof(buf), t->hash))
 		return (free(t), NULL);
+
 	return (t);
 }
