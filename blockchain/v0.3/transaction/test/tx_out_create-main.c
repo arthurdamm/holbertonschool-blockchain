fbc7a08991c3f602e1ad07278dfdf7faba332303
Arthur Damm <indigoarthur@gmail.com>
2020-10-24 19:06:36 +0000

    Add task 2

--- blockchain/v0.3/transaction/test/tx_out_create-main.c
+++ blockchain/v0.3/transaction/test/tx_out_create-main.c
@@ -37,17 +37,17 @@ static void _tx_out_print(tx_out_t const *out)
  */
 int main(void)
 {
-	EC_KEY *receiver;
-	uint8_t receiver_pub[EC_PUB_LEN];
+	
 	tx_out_t *out;
+	const uint8_t *str = (unsigned char *)"\x04\x28\x35\x90\x8c\x04\xf2\xb9\xa7\x51\x53\xda\x5e\x4a\x46\x60\x22\x2a\x59\x70\xcf\xdd\xa9\x2a\x1a\xbb\x38\xc1\x40\xa2\xd6\x5f\x7e\xf6\xe5\x56\x63\x28\x4f\x8a\x5b\xbc\x7b\x6e\xb8\xe7\x8f\x0b\x98\xcd\x2b\x27\x90\xdf\x2b\x63\xc6\x55\x1f\x7c\xb6\xa9\xfa\x9a\xeb";
+	
 
-	receiver = ec_create();
-
-	out = tx_out_create(972, ec_to_pub(receiver, receiver_pub));
+	
+	out = tx_out_create(972, str);
 	_tx_out_print(out);
 
 	free(out);
-	EC_KEY_free(receiver);
+	
 
 	return (EXIT_SUCCESS);
 }
