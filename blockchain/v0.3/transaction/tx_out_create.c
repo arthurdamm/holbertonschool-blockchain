0f2872ff78922a8ca4db8d73c2b882ef1bd64fa3
Arthur Damm <indigoarthur@gmail.com>
2020-10-24 17:30:28 +0000

    Use original code with correct address...

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -9,16 +9,12 @@
 tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 {
 	tx_out_t *t = calloc(1, sizeof(*t));
-	unsigned char buf[sizeof(t->amount) + sizeof(t->pub)];
 
 	if (!t)
 		return (NULL);
 	t->amount = amount;
 	memcpy(t->pub, pub, sizeof(t->pub));
-	memcpy(buf, &amount, sizeof(amount));
-	memcpy(buf + sizeof(amount), pub, EC_PUB_LEN);
-	if (!sha256((int8_t const *)buf, sizeof(buf), t->hash))
+	if (!sha256((int8_t const *)t, sizeof(t->amount) + sizeof(t->pub), t->hash))
 		return (free(t), NULL);
-
 	return (t);
 }
