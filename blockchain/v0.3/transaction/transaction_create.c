fe1671b0c9963c40b0103e8091539f749941c3ef
Arthur Damm <indigoarthur@gmail.com>
2020-10-25 03:55:18 +0000

    Initial task 5

---
+++ blockchain/v0.3/transaction/transaction_create.c
@@ -0,0 +1,101 @@
+#include "transaction.h"
+
+typedef struct Visitor
+{
+	llist_t *sender_unspent;
+	uint8_t *sender_pub;
+	uint64_t total_amount;
+	uint64_t amount;
+
+} visitor_t;
+
+int collect_sender_unspent(llist_node_t node, unsigned int idx, void *arg)
+{
+	visitor_t *visitor = arg;
+	unspent_tx_out_t *utx = node;
+
+	if (!memcmp(utx->out.pub, visitor->sender_pub, EC_PUB_LEN))
+	{
+		if (llist_add_node(visitor->sender_unspent, node, ADD_NODE_REAR))
+			exit(1);
+		visitor->total_amount += utx->out.amount;
+		if (visitor->total_amount >= visitor->amount)
+			return (0);
+	}
+	return (0);
+	(void)idx;
+}
+
+int map_output_to_input(llist_node_t node, unsigned int idx, void *arg)
+{
+	llist_t *inputs = arg;
+	unspent_tx_out_t *utx = node;
+	tx_in_t *txi = tx_in_create(utx);
+
+	if (!txi)
+		exit(1);
+	if (llist_add_node(inputs, txi, ADD_NODE_REAR))
+		exit(1);
+	return (0);
+	(void)idx;
+}
+
+/**
+ * transaction_create - creates a transaction
+ * @sender: private key of tx sender
+ * @receiver: public key of tx receiver
+ * @amount: tx amount
+ * @all_unspent: llist of unspent outputs to date
+ * Return: pointer to new transaction struct or NULL
+ */
+transaction_t *transaction_create(EC_KEY const *sender, EC_KEY const *receiver,
+	uint32_t amount, llist_t *all_unspent)
+{
+	ssize_t i;
+	uint8_t sender_pub[EC_PUB_LEN], receiver_pub[EC_PUB_LEN];
+	llist_t *sender_unspent;
+	visitor_t visitor = {0};
+	transaction_t *tx;
+	tx_out_t *to_receiver, *to_sender;
+
+	if (!sender || !receiver || !amount || !all_unspent)
+		return (NULL);
+	if (!ec_to_pub(sender, sender_pub) || !ec_to_pub(receiver, receiver_pub))
+		return (NULL);
+	sender_unspent = llist_create(MT_SUPPORT_FALSE);
+	if (!sender_unspent)
+		return (NULL);
+	visitor.sender_unspent = sender_unspent;
+	visitor.amount = amount;
+	visitor.sender_pub = sender_pub;
+	llist_for_each(all_unspent, collect_sender_unspent, &visitor);
+	/* printf("SIZE:[%d] amount: [%ld]\n", llist_size(sender_unspent), visitor.total_amount); */
+	if (visitor.total_amount < amount)
+		return (NULL);
+	tx = calloc(1, sizeof(*tx));
+	if (!tx)
+		return (NULL);
+	tx->inputs = llist_create(MT_SUPPORT_FALSE);
+	tx->outputs = llist_create(MT_SUPPORT_FALSE);
+	to_receiver = tx_out_create(amount, receiver_pub);
+	to_sender = visitor.total_amount > amount ?
+		tx_out_create(visitor.total_amount - amount, sender_pub) : 0;
+	if (!tx->inputs || !tx->outputs || !to_receiver ||
+		(visitor.total_amount > amount && !to_sender))
+		return (llist_destroy(tx->inputs, 1, free),
+			llist_destroy(tx->outputs, 1, free), free(tx), NULL);
+	llist_for_each(sender_unspent, map_output_to_input, tx->inputs);
+	if (llist_add_node(tx->outputs, to_receiver, ADD_NODE_REAR) ||
+		(to_sender && llist_add_node(tx->outputs, to_sender, ADD_NODE_REAR)))
+		exit(1);
+	if (!transaction_hash(tx, tx->id))
+		exit(1);
+	for (i = 0; i < llist_size(tx->inputs); i++)
+	{
+		tx_in_t *in = llist_get_node_at(tx->inputs, i);
+
+		if (!tx_in_sign(in, tx->id, sender, all_unspent))
+			exit(1);
+	}
+	return (tx);
+}
