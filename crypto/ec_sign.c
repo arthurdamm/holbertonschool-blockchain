1c17bfdd8072282717fccd2818c979c638befbb4
Arthur Damm <indigoarthur@gmail.com>
2020-10-25 14:23:08 +0000

    Use simple ECDSA_sign

--- crypto/ec_sign.c
+++ crypto/ec_sign.c
@@ -11,40 +11,10 @@
 uint8_t *ec_sign(EC_KEY const *key, uint8_t const *msg, size_t msglen,
 	sig_t *sig)
 {
-	EVP_MD_CTX *ctx = NULL;
-	EVP_PKEY *pkey = NULL;
-	int ret;
-
 	if (!key || !msg || !sig)
 		return (NULL);
-	pkey = EVP_PKEY_new();
-	if (!pkey)
+	if (ECDSA_sign(0, msg, msglen, sig->sig, (unsigned int *)&sig->len,
+		(EC_KEY *)key) != 1)
 		return (NULL);
-	if (EVP_PKEY_set1_EC_KEY(pkey, (struct ec_key_st *)key) != 1)
-		return (EVP_PKEY_free(pkey), NULL);
-	ctx = EVP_MD_CTX_create();
-	if (!ctx)
-		return (EVP_PKEY_free(pkey), NULL);
-	if (EVP_DigestSignInit(ctx, NULL, EVP_sha256(), NULL, pkey) != 1)
-	{
-		EVP_PKEY_free(pkey), EVP_MD_CTX_destroy(ctx);
-		return (NULL);
-	}
-	if (EVP_DigestSignUpdate(ctx, msg, msglen) != 1)
-	{
-		EVP_PKEY_free(pkey), EVP_MD_CTX_destroy(ctx);
-		return (NULL);
-	}
-	sig->len = SIG_MAX_LEN;
-	ret = EVP_DigestSignFinal(ctx, sig->sig, (size_t *)&(sig->len));
-	if (ret != 1)
-	{
-		/* dprintf(2, "ERR [%s]\n", ERR_error_string(ERR_get_error(), NULL)); */
-		EVP_PKEY_free(pkey), EVP_MD_CTX_destroy(ctx);
-		return (NULL);
-	}
-
-	EVP_PKEY_free(pkey);
-	EVP_MD_CTX_destroy(ctx);
 	return (sig->sig);
 }
